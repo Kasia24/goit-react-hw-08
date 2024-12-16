7e96becfdfe64a7edeb3e5f8a702b1d1f0f8c036
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-16 19:16:55 +0100

    add

--- src/redux/LoginUser.js
+++ src/redux/LoginUser.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const API_URL = "https://connections-api.goit.global/";
 
-export const loginUser = async ({ username, password }) => {
+export const loginUser = async ({ email, password }) => {
   const response = await axios.post(`${API_URL}users/login`, {
-    username,
+    email,
     password,
   });
   return response.data; // Zakładając, że response.data zawiera { user, token }
